d7b31afc945be9ce26c6baaba792a9d549cec232
pavank0723 <pavan.yadav0723@gmail.com>
2022-08-10 18:34:58 +0530

    BrowserRoute path change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import {Routes,Route} from 'react-router-dom';
 import Error from './pages/Error';
 import Home from './pages/Home';
 import SingleMovie from './pages/SingleMovie';
@@ -8,13 +8,13 @@ import SingleMovie from './pages/SingleMovie';
 const App = () => {
   return (
     <>
-      <BrowserRouter>
+      
         <Routes>
           <Route path='/' element = {<Home />}/>
           <Route path='movie/:id' element = {<SingleMovie />}/>
           <Route path='*' element={<Error />}/>
         </Routes>
-      </BrowserRouter>
+      
     </>
   )
 }
