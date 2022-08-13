11aff28d0d305d4a38fb0ed6f68cf655d3b7135d
pavank0723 <pavan.yadav0723@gmail.com>
2022-08-13 20:13:38 +0530

    add debouncing concept and delay time

--- src/pages/context.js
+++ src/pages/context.js
@@ -2,7 +2,7 @@
 // Provider (Delivery Boy)
 // Consumer or useContext() (Get Data)
 
-import React,{useContext, useEffect, useState} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 const API_URL = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
 
@@ -11,38 +11,43 @@ const AppContext = React.createContext();
 // need to create a provider function
 const AppProvider = ({ children }) => {
 
-    const [isLoading,setIsLoading] = useState(true);
-    const [movie,setMovie] = useState([]);
-    const [isError,setIsError] = useState({show:"false",msg:""});
-    const [query,setQuery] = useState("titanic");
+    const [isLoading, setIsLoading] = useState(true);
+    const [movie, setMovie] = useState([]);
+    const [isError, setIsError] = useState({ show: "false", msg: "" });
+    const [query, setQuery] = useState("titanic");
 
-    const getMovies = async (url) =>{ //parameter
-        try{
+    const getMovies = async (url) => { //parameter
+        try {
             const reqData = await fetch(url);
             const resData = await reqData.json();
             console.log(resData);
 
-            if(resData.Response === "True"){
+            if (resData.Response === "True") {
                 setIsLoading(false);
                 setMovie(resData.Search);
-            }else{
+            } else {
                 setIsError({
-                    show:"true",
-                    msg:resData.error
+                    show: "true",
+                    msg: resData.error
                 }
                 )
             }
-        }catch(error){
+        } catch (error) {
             console.log(error);
 
         }
     }
 
     useEffect(() => {
-        getMovies(`${API_URL}&s=${query}`); //Argument
-    },[query]);
+        let waitTime = setTimeout(() => {
+            getMovies(`${API_URL}&s=${query}`); //Argument
+        }, 700);
 
-    return <AppContext.Provider value={{isLoading,isError,movie,query,setQuery}}>
+        return () => clearTimeout(waitTime);
+
+    }, [query]);
+
+    return <AppContext.Provider value={{ isLoading, isError, movie, query, setQuery }}>
         {children}
     </AppContext.Provider>
 };
@@ -52,4 +57,4 @@ const useGlobalContext = () => {
     return useContext(AppContext)
 }
 
-export { AppContext, AppProvider,useGlobalContext };
+export { AppContext, AppProvider, useGlobalContext };
